80c40f7dd0a68b015664ac3611386d0fd589c7ad
jkalayan <jkalayan@gmail.com>
2026-09-21 17:25:46 +0100

    update .py to work as https://github.com/CompBioAsia/mdme/blob/main/mesmy.sh, reduce nsteps for eq and prod steps

--- AMBER/mesmy.py
+++ AMBER/mesmy.py
@@ -24,217 +24,198 @@ def mesmy_cli():
     nres = t_solute.n_residues
     script = f"""#!/bin/bash
 
-    # An equilibration workflow.
-    # Designed for "standard" protein or protein/ligand systems
-    # in explicit solvent. Not optimised for membrane protein systems.
-    #
-    # Developed from scripts provided by the Hughes lab at the University
-    # of Montana.
-    #
 
-    # You may wish to modify some of the parameters below.
-    prmtop_file="{args.prmtop}"
-    inpcrd_file="{args.inpcrd}"
-    solute={nres} # number of residues in solute
-    T="310.0" # target temperature in K
-    PMEMD="pmemd.cuda" # name of your MD executable (e.g. may be "pmemd.MPI")
+# An equilibration workflow.
+# Designed for "standard" protein or protein/ligand systems
+# in explicit solvent. Not optimised for membrane protein systems.
+#
+# Developed from scripts provided by the Hughes lab at the University
+# of Montana.
+#
 
-    ### DO NOT MODIFY BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING ###
-    # 1K step energy minimization, strong restraints on heavy atoms, no shake
-    cat > step1.in <<EOF
-    Min with strong restraints on heavy atoms, no shake
-    &cntrl
-    imin = 1, ncyc = 50, maxcyc = 1000,
-    ntpr = 50,
-    ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 5.0,
-    &end
-    EOF
+# You may wish to modify some of the parameters below.
+prmtop_file="abl_ligand.prmtop"
+inpcrd_file="abl_ligand.inpcrd"
+solute=271 # number of residues in solute
+T="310.0" # target temperature in K
+#PMEMD="pmemd.cuda" # name of your MD executable (e.g. may be "pmemd.MPI")
+PMEMD="mpirun -n $SLURM_NTASKS sander.MPI" # name of your MD executable (e.g. may be "pmemd.MPI")
 
-    # NVT MD with strong restraints on heavy atoms, shake, dt=.001, 15 ps
-    cat > step2.in <<EOF
-    NVT MD with strong restraints on heavy atoms, shake dt 0.001 15 ps
-    &cntrl
-    imin = 0, nstlim = 30000, dt=0.001,
-    ntpr = 50, ntwr = 500,
-    iwrap = 1,
-    ntc = 2, ntf = 2,
-    ntt = 3, gamma_ln = 5, temp0 = $T, tempi = $T,
-    ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 5.0,
-    &end
-    EOF
+### DO NOT MODIFY BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING ###
 
-    # Energy minimization with relaxed restraints on heavy atoms, no shake
-    cat > step3.in <<EOF
-    Min with relaxed restraints on heavy atoms
-    &cntrl
-    imin = 1, ncyc = 50, maxcyc = 1000,
-    ntpr = 50,
-    ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 2.0,
-    &end
-    EOF
 
-    # Energy minimization with minimal restraints on heavy atoms, no shake
-    cat > step4.in <<EOF
-    Min with minimal restraints on heavy atoms
-    &cntrl
-    imin = 1, ncyc = 50, maxcyc = 1000,
-    ntpr = 50,
-    ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 0.1,
-    &end
-    EOF
 
-    # Energy minimization with no restraints, no shake
-    cat > step5.in <<EOF
-    Min with no restraints
-    &cntrl
-    imin = 1, ncyc = 50, maxcyc = 1000,
-    ntpr = 50,
-    &end
-    EOF
+# 1K step Steepest Descent Minimization with strong restraints on heavy atoms, no shake
+cat > step1.in <<EOF
+Min explicit solvent heavy atom rest no shake
+&cntrl
+  imin = 1, ntmin = 2, maxcyc = 2000,
+  ntwx = 1000, ioutfm = 1, ntpr = 50, ntwr = 500,
+  ntc = 1, ntf = 1, ntb = 1, cut = 8.0,
+  igb = 0, saltcon = 0.0,
+  ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 5.0,
+&end
+EOF
 
-    # NPT MD with shake and low restraints on heavy atoms, 20 ps dt=.002
-    cat > step6.in <<EOF
-    NPT MD with shake and low restraints on heavy atoms, 20 ps dt=.002
-    &cntrl
-    imin = 0, nstlim = 10000, dt = 0.002,
-    ntwx = 1000, ntpr = 50, ntwr = 500,
-    iwrap = 1,
-    ntc = 2, ntf = 2, ntb = 2,
-    ntt = 3, gamma_ln = 5, temp0 = $T, tempi = $T,
-    ntp = 1, taup = 1.0,
-    ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 1.0,
-    &end
-    EOF
+# NTV MD with strong restraints on heavy atoms, shake, dt=.001, 15 ps
+cat > step2.in <<EOF
+MD explicit solvent heavy atom rest shake dt 0.001
+&cntrl
+  imin = 0, nstlim = 30000, dt=0.001,
+  ntx = 1, irest = 0, ig = -1,
+  ntwx = 1500, ioutfm = 1, ntpr = 50, ntwr = 500,
+  iwrap = 1, nscm = 0,
+  ntc = 2, ntf = 1, ntb = 1, cut = 8.0,
+  ntt = 1, tautp = 0.5, temp0 = $T, tempi = $T,
+  ntp = 0, taup = 0.5,
+  igb = 0, saltcon = 0.0,
+  ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 5.0,
+&end
+EOF
 
-    # NPT MD with shake and minimal restraints on heavy atoms
-    cat > step7.in <<EOF
-    NPT MD with minimal restraints on heavy atoms, 20 ps dt=.002
-    &cntrl
-    imin = 0, nstlim = 10000, dt=0.002,
-    ntx = 5, irest = 1,
-    ntwx = 1000, ntpr = 50, ntwr = 500,
-    iwrap = 1,
-    ntc = 2, ntf = 2, ntb = 2,
-    ntt = 3, gamma_ln = 5, temp0 = $T, tempi = $T,
-    ntp = 1, taup = 1.0,
-    ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 0.5,
-    &end
-    EOF
+# Steepest Descent Minimization with relaxed restraints on heavy atoms, no shake
+cat > step3.in <<EOF
+Min explicit solvent relaxed heavy atom rest no shake
+&cntrl
+  imin = 1, ntmin = 2, maxcyc = 2000,
+  ntwx = 1000, ioutfm = 1, ntpr = 50, ntwr = 500,
+  ntc = 1, ntf = 1, ntb = 1, cut = 8.0,
+  igb = 0, saltcon = 0.0,
+  ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 2.0,
+&end
+EOF
 
-    # NPT MD, shake and minimal restraints on backbone atoms, dt=0.002, 20 ps
-    cat > step8.in <<EOF
-    NPT MD with minimal restraints on backbone atoms, 20 ps dt=.002
-    &cntrl
-    imin = 0, nstlim = 10000, dt=0.002,
-    ntx = 5, irest = 1,
-    ntwx = 1000, ntpr = 50, ntwr = 500,
-    iwrap = 1,
-    ntc = 2, ntf = 2, ntb = 2,
-    ntt = 3, gamma_ln = 5, temp0 = $T, tempi = $T,
-    ntp = 1, taup = 1.0,
-    ntr = 1, restraintmask = ":1-$solute@H,N,CA,HA,C,O", restraint_wt = 0.5,
-    &end
-    EOF
+# Steepest Descent Minimization with minimal restraints on heavy atoms, no shake
+cat > step4.in <<EOF
+Min explicit solvent minimal heavy atom rest no shake
+&cntrl
+  imin = 1, ntmin = 2, maxcyc = 2000,
+  ntwx = 1000, ioutfm = 1, ntpr = 50, ntwr = 500,
+  ntc = 1, ntf = 1, ntb = 1, cut = 8.0,
+  igb = 0, saltcon = 0.0,
+  ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 0.1,
+&end
+EOF
 
-    # NPT MD with shake and no restraints, dt=0.002, 200 ps
-    cat > step9.in <<EOF
-    NPT MD no restraints, dt=0.002, 200 ps
-    &cntrl
-    imin = 0, nstlim = 10000, dt=0.002,
-    ntx = 5, irest = 1,
-    ntwx = 1000, ntpr = 50, ntwr = 500,
-    iwrap = 1,
-    ntc = 2, ntf = 2, ntb = 2,
-    ntt = 3, gamma_ln = 5, temp0 = $T, tempi = $T,
-    ntp = 1, taup = 1.0,
-    &end
-    EOF
+# Steepest Descent Minimization with no restraints, no shake
+cat > step5.in <<EOF
+Min explicit solvent no heavy atom res no shake
+&cntrl
+  imin = 1, ntmin = 2, maxcyc = 2000,
+  ntwx = 1000, ioutfm = 1, ntpr = 50, ntwr = 500,
+  ntc = 1, ntf = 1, ntb = 1, cut = 8.0,
+  igb = 0, saltcon = 0.0,
+  ntr = 0,
+&end
+EOF
 
-    START="`date +%s.%N`"
+# NTP MD with shake and low restraints on heavy atoms, 5 ps dt=.001
+cat > step6.in <<EOF
+MD explicit solvent heavy atom low rest shake dt 0.001
+&cntrl
+  imin = 0, nstlim = 10000, dt=0.001,
+  ntx = 1, irest = 0, ig = -1,
+  ntwx = 1000, ioutfm = 1, ntpr = 50, ntwr = 500,
+  iwrap = 1, nscm = 0,
+  ntc = 2, ntf = 1, ntb = 2, cut = 8.0,
+  ntt = 1, tautp = 1.0, temp0 = $T, tempi = $T,
+  ntp = 1, taup = 1.0,
+  igb = 0, saltcon = 0.0,
+  ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 1.0,
+&end
+EOF
 
-    # Minimization Phase - reference coords are updated each run
-    for RUN in step1 step2 step3 step4 step5 ; do
-    echo "------------------------"
-    echo "Minimization phase: $RUN"
-    echo "------------------------"
-    if [[ ! -f $RUN.out ]]; then
-        echo "File -- $RUN.out -- does not exists. Running job..."
-        runme=1
-    else
-        grep -q 'Total wall time' $RUN.out
-        retval=$?
-        if [ $retval -ne 0 ]; then
-        echo "$RUN did not complete last time. Trying again..."
-        runme=1
-        else
-            echo "$RUN has already completed.  Checking the next step."
-            runme=0
-        fi
-    fi
+# NTP MD with shake and minimal restraints on heavy atoms
+cat > step7.in <<EOF
+MD explicit solvent heavy atom minimal rest shake dt 0.001, 10 ps, dt=.001
+&cntrl
+  imin = 0, nstlim = 10000, dt=0.001,
+  ntx = 5, irest = 1,
+  ntwx = 1000, ioutfm = 1, ntpr = 50, ntwr = 500,
+  iwrap = 1, nscm = 0,
+  ntc = 2, ntf = 1, ntb = 2, cut = 8.0,
+  ntt = 1, tautp = 1.0, temp0 = $T, tempi = $T,
+  ntp = 1, taup = 1.0,
+  igb = 0, saltcon = 0.0,
+  ntr = 1, restraintmask = ':1-$solute & !@H=', restraint_wt = 0.5,
+&end
+EOF
 
-    if [ $runme -eq 1 ]; then
-        $PMEMD -O -i $RUN.in -p $prmtop_file -c $inpcrd_file \
-        -ref $inpcrd_file -o $RUN.out -x $RUN.nc -r $RUN.ncrst \
-            -inf $RUN.mdinfo
-        grep -q 'Total wall time' $RUN.out
-        retval=$?
-        if [ $retval -ne 0 ]; then
-            echo "Error - job failed at this step."
-        exit $retval
-        fi
-    fi
+# NTP MD with shake and minimal restraints on backbone atoms, dt=0.001, 10 ps
+cat > step8.in <<EOF
+MD explicit solvent heavy atom minimal BB rest shake dt 0.001
+&cntrl
+  imin = 0, nstlim = 10000, dt=0.001,
+  ntx = 5, irest = 1,
+  ntwx = 1000, ioutfm = 1, ntpr = 50, ntwr = 500,
+  iwrap = 1, nscm = 0,
+  ntc = 2, ntf = 1, ntb = 2, cut = 8.0,
+  ntt = 1, tautp = 1.0, temp0 = $T, tempi = $T,
+  ntp = 1, taup = 1.0,
+  igb = 0, saltcon = 0.0,
+  ntr = 1, restraintmask = ":1-$solute@H,N,CA,HA,C,O", restraint_wt = 0.5,
+&end
+EOF
 
-    echo ""
-    inpcrd_file="$RUN.ncrst"
-    done
+# NTP MD with shake and no restraints, dt=0.002, 1 ns
+cat > step9.in <<EOF
+MD explicit solvent heavy atom no rest shake dt 0.002
+&cntrl
+  imin = 0, nstlim = 10000, dt=0.002,
+  ntx = 5, irest = 1,
+  ntwx = 500, ioutfm = 1, ntpr = 1000, ntwr = 1000,
+  iwrap = 1, nscm = 1000,
+  ntc = 2, ntf = 1, ntb = 2, cut = 8.0,
+  ntt = 1, tautp = 1.0, temp0 = $T, tempi = $T,
+  ntp = 1, taup = 1.0,
+  igb = 0, saltcon = 0.0,
+  ntr = 0,
+&end
+EOF
 
-    # Equilibration phase: reference coords are last coords from minimize phase
-    REF=$inpcrd_file
-    for RUN in step6 step7 step8 step9 ; do
+START="`date +%s.%N`"
 
-    echo "------------------------"
-    echo "Equilibration phase: $RUN"
-    echo "------------------------"
-    if [[ ! -f $RUN.out ]]; then
-        echo "File -- $RUN.out -- does not exists. Running job..."
-        runme=1
-    else
-        grep -q 'Total wall time' $RUN.out
-        retval=$?
-        if [ $retval -ne 0 ]; then
-        echo "$RUN did not complete last time. Trying again..."
-        runme=1
-        else
-            echo "$RUN has already completed.  Checking the next step."
-            runme=0
-        fi
-    fi
+# Minimization Phase
+for RUN in step1 step2 step3 step4 step5 ; do
+ echo "------------------------"
+ echo "Minimization phase: $RUN"
+ echo "------------------------"
+ if [[ ! -f $RUN.rst7 ]]; then
+     echo "File -- $RUN.rst7 -- does not exists. Running job..."
+     $PMEMD -O -i $RUN.in -p $prmtop_file -c $inpcrd_file -ref $inpcrd_file -o $RUN.out -x $RUN.nc -r $RUN.rst7 -inf $RUN.mdinfo
+ else
+     echo "File -- $RUN.rst7 -- exists.  Checking the next step."
+ fi
+ echo ""
+ inpcrd_file="$RUN.rst7"
+done
 
-    if [ $runme -eq 1 ]; then
-        $PMEMD -O -i $RUN.in -p $prmtop_file -c $inpcrd_file\
-        -ref $REF -o $RUN.out -x $RUN.nc -r $RUN.ncrst -inf $RUN.mdinfo
-        grep -q 'Total wall time' $RUN.out
-        retval=$?
-        if [ $retval -ne 0 ]; then
-            echo "Error - job failed at this step."
-        exit $retval
-        fi
-    fi
+# Equilibration phase - reference coords are last coords from minimize phase
+REF=$inpcrd_file
+for RUN in step6 step7 step8 step9 ; do
 
-    echo ""
-    inpcrd_file="$RUN.ncrst"
-    done
+ echo "------------------------"
+ echo "Equilibration phase: $RUN"
+ echo "------------------------"
+ if [[ ! -f $RUN.rst7 ]]; then
+     echo "File -- $RUN.rst7 -- does not exists. Running job..."
+     $PMEMD -O -i $RUN.in -p $prmtop_file -c $inpcrd_file -ref $REF -o $RUN.out -x $RUN.nc -r $RUN.rst7 -inf $RUN.mdinfo
+ else
+     echo "File -- $RUN.rst7 -- exists.  Checking the next step."
+ fi
+  echo ""
+  inpcrd_file="$RUN.rst7"
 
-    # Reset the time in the restart file to zero:
-    sed -i 's/0.3000000E+02/0.0000000E+00/g' step9.ncrst
+done
 
-    STOP="`date +%s.%N`"
-    # The divide by 1 in the line below is to overcome a bug in bc...
-    TIMING=`echo "scale=1; ($STOP - $START) / 1;" | bc`
-    echo "Total run time: $TIMING seconds."
-    echo ""
+sed -i 's/0.3000000E+02/0.0000000E+00/g' step9.rst7
 
-    exit 0
+STOP="`date +%s.%N`"
+TIMING=`echo "scale=4; $STOP - $START;" | bc`
+echo "$TIMING seconds."
+echo ""
+
+exit 0    
     """
     print(script)
 
