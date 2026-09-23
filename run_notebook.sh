548704511132233456fc462242703874693fb6c3
Jas Kalayan <jkalayan@gmail.com>
2026-09-23 10:53:37 +0100

    tidy up env setup script

--- run_notebook.sh
+++ run_notebook.sh
@@ -55,7 +55,7 @@ cd "$(dirname "$0")"
 # Choose a name for the new environment
 # Note that deleting this will not clean up the micromamba cache, which may be
 # substantial.
-ENVNAME='cba_md_25b'
+ENVNAME='protein_sim_env'
 
 # The filename of the Conda environment file to run the notebook in
 ENV_FILE='environment.yaml'
